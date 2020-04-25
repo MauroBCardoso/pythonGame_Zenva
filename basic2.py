d270760383c7c5128cd9c27acd5e077d8ad249c8
mb_cardoso <mb.cardoso@campus.fct.unl.pt>
2020-04-25 15:50:33 +0100

    Python basics (operations)

---
+++ basic2.py
@@ -0,0 +1,28 @@
+#Python basics (operations)
+
+#string
+name = "Mauro"
+
+#int
+x_pos = 5
+
+#float
+speed = 2.5 
+
+#bool
+is_game_over = False
+
+
+x_pos = speed #x_pos = 2.5
+
+is_not_game_over = not is_game_over #negate is_game_over
+
+new_x_pos = x_pos + speed  #new_x_pos = 5
+
+full_name = name + ' Cardoso' #full_name = Mauro Cardoso
+
+x_pos = 5
+mod_x_pos = 5%2 #mod_x_pos = 1 (resto da divisao)
+floor_div_x_pos = x_pos // 2 #floor_div_x_pos = 2
+x_pos_squared = x_pos**2  #x_pos_squared = 25
+
