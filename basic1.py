5aecdfcb4853e468252505567621f0f3664e34ba
mb_cardoso <mb.cardoso@campus.fct.unl.pt>
2020-04-25 15:49:34 +0100

    Python basics (variables syntax)

---
+++ basic1.py
@@ -0,0 +1,18 @@
+#Python basics (variables syntax)
+
+name = "Mauro"
+
+#int
+x_pos = 5
+
+#float
+speed = 2.5 
+
+#bool
+is_game_over = False
+
+print(type(x_pos))
+
+x_pos = 'hhh'
+
+print(type(x_pos))
